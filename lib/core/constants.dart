81a395e2e832bddacf3fc36afaa271b0db039b16
Hirushan Subasinghe <lakshithasubasinghe8@gmail.com>
2025-04-21 19:57:01 +0530

    settings gui add

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const String baseUrl = "http://192.168.8.191:5000"; // or localhost IP
+const String baseUrl = "http://10.105.73.117:5000"; // or localhost IP
 
 class AppColors {
   static const Color primaryColor = Color(0xFF2C5DE1);
